e111325f3b1bb11c23626eca5d36c07748f61840
tuandm1905 <tuandm1905@gmail.com>
2024-07-29 03:24:31 +0700

    chat, guestconsultation

---
+++ src/app/owner/pages/guestconsultation-page/guestconsultation-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guestconsultation-page',
+  templateUrl: './guestconsultation-page.component.html',
+  styleUrl: './guestconsultation-page.component.scss'
+})
+export class GuestconsultationPageComponent {
+
+}
