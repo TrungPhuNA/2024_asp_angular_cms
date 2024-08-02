a94982df31caa87bc634d365ae9aef802b4d38cf
tuandm1905 <tuandm1905@gmail.com>
2024-08-02 23:38:48 +0700

    update import product

--- src/app/owner/pages/importproduct-page/importproduct-page.component.ts
+++ src/app/owner/pages/importproduct-page/importproduct-page.component.ts
@@ -1,39 +1,80 @@
 import { Component } from '@angular/core';
-interface Product {
-  code: string;
-  name: string;
-  quantity: number;
-  price: number;
-}
+import { FormControl, FormGroup } from '@angular/forms';
+import { INIT_PAGING } from '../../helpers/constant';
+import { AlertService } from '../../helpers/alert.service';
+import { OwnerService } from '../../services/owner.service';
+import { AuthenService } from '../../../admin/services/authen.service';
 
-interface OrderDetail {
-  productCode: string;
-  productName: string;
-  quantity: number;
-  price: number;
-}
 @Component({
   selector: 'app-importproduct-page',
   templateUrl: './importproduct-page.component.html',
   styleUrl: './importproduct-page.component.scss'
 })
 export class ImportproductPageComponent {
-  products: Product[] = [
-    { code: 'LP13', name: 'Laptop HP 15s-fq2663TU', quantity: 19, price: 9990000 },
-    { code: 'LP14', name: 'Laptop Lenovo IdeaPad 5 Pro 16IAH7', quantity: 3, price: 25190000 },
-    // Thêm các sản phẩm khác ở đây
-  ];
+  dataList: any = [];
+  modalTitle: string = '';
+  showModal: boolean = false;
+	openModal: boolean = false;
+  userType: string = '';
+	pageName: string = 'accounts';
+	paging: any = { ...INIT_PAGING }
+	loading = false;
+  selected: any;
 
-  orderDetails: OrderDetail[] = [
-    { productCode: 'LP13', productName: 'Laptop HP 15s-fq2663TU', quantity: 4, price: 9990000 },
-    { productCode: 'LP15', productName: 'Laptop Lenovo IdeaPad 5 Pro 16IAH7', quantity: 1, price: 25190000 },
-    // Thêm chi tiết đơn hàng khác ở đây
-  ];
+	typeForm = 0;
+  breadCrumb: any = [
+		{ label: 'Owner', link: '/' },
+		{ label: 'Import Product', link: '/owner/importproduct' }
+	];
+  constructor(
+		private alertService: AlertService,
+		private ownerService: OwnerService,
+		private authenService: AuthenService,
+	) {
 
-  displayedColumns: string[] = ['code', 'name', 'quantity', 'price'];
-  orderDisplayedColumns: string[] = ['stt', 'productCode', 'productName', 'quantity', 'price'];
+	}
+  dataListAll = [];
 
-  get totalAmount() {
-    return this.orderDetails.reduce((total, detail) => total + (detail.quantity * detail.price), 0);
-  }
+
+  editItem(id: number) {
+		const data = this.dataList.find((c: any) => c.adId === id);
+		this.selected = { ...data };
+		this.modalTitle = 'Edit Blog';
+		this.openModal = true;
+		this.typeForm = 3;
+
+	}
+  formSearch: any = new FormGroup({
+		id: new FormControl(null),
+		name: new FormControl(null)
+	});
+  createItem() {
+		this.modalTitle = 'Import Product';
+		this.openModal = true;
+		this.typeForm = 1;
+	}
+  search() {
+		// this.pageChanged(1);
+		// this.getDataList({ ...this.paging, page: 1, ...this.formSearch.value })
+	}
+  resetSearchForm() {
+		this.formSearch.reset();
+		this.search();
+	}
+  pageChanged(e: any) {
+		this.paging.page = e;
+		// this.getDataList({ ...this.paging, ...this.formSearch.value })
+		if (this.dataListAll?.length > 0) {
+			let start = (this.paging?.page - 1) * this.paging.pageSize;
+			let end = this.paging?.page * this.paging.pageSize;
+			if (this.formSearch.value?.name) {
+				let totalSearch = this.dataListAll?.filter((item: any) => item?.title?.toLowerCase()?.includes(this.formSearch.value?.name?.toLowerCase().trim()));
+				this.paging.total = totalSearch?.length || 0;
+				this.dataList = totalSearch?.filter((item: any, index: number) => index >= start && index < end && item?.title?.toLowerCase()?.includes(this.formSearch.value?.name?.toLowerCase().trim()))
+			} else {
+				this.dataList = this.dataListAll?.filter((item: any, index: number) => index >= start && index < end)
+			}
+			// this.dataList = this.dataListAll?.filter((item: any, index: number) => index >= start && index < end)
+		}
+	}
 }
